76a6adee15f9508381d0f4fd9bca79bdeb07ccf1
Leigh <351529+leighmcculloch@users.noreply.github.com>
2025-10-15 20:20:26 +1000

    Update mcp-stellar-meta.js

--- mcp-stellar-meta.js
+++ mcp-stellar-meta.js
@@ -255,9 +255,11 @@ async function getLedger(s3Client, ledgerSequence) {
   const cacheUrl = `http://localhost/${ledgerSequence}.xdr`;
   const cached = await cache.match(cacheUrl);
   if (cached) {
+    console.log(`using ledger ${ledgerSequence} from cache`);
     ledgerXdr = Buffer.from(await cached.arrayBuffer());
   } else {
     const key = getPathForLedger(ledgerSequence);
+    console.log(`fetching ledger ${ledgerSequence} from ${AWS_BUCKET} with key ${key}`);
     const getObjectParams = {
       Bucket: AWS_BUCKET,
       Key: key,
